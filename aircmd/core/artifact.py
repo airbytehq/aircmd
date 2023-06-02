c913078452bc637aeb4ca9e8689fa449aa592460
cpdeethree <conor@airbyte.io>
2023-06-02 17:38:34 -0500

    new build command

--- aircmd/core/artifact.py
+++ aircmd/core/artifact.py
@@ -1,12 +1,24 @@
 from typing import List
 
-from ..models.click_commands import ClickArgument, ClickCommandMetadata
+from ..models.base import PipelineContext
+from ..models.click_commands import ClickArgument, ClickCommandMetadata, ClickGroup
+from ..models.utils import make_pass_decorator
 
+pass_pipeline_context = make_pass_decorator(PipelineContext, ensure=True)
+
+plugin_group = ClickGroup(group_name="core", group_help="Commands for developing aircmd")
 
 class BuildCommand(ClickCommandMetadata):
     command_name: str = "build"
     command_help: str = "Build aircmd"
-    arguments: List[ClickArgument] = [ClickArgument(name="query", required=False)]
+
+@plugin_group.command(BuildCommand())
+@pass_pipeline_context
+async def build(ctx: PipelineContext) -> None:
+    """Build aircmd"""
+    async with ctx.dagger_connection as client:
+
+
 
 class TestCommand(ClickCommandMetadata):
     command_name: str = "test"
