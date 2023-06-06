d2b3d41a92cf01f7dbd06b4290d4114955d46722
cpdeethree <conor@airbyte.io>
2023-06-06 17:46:46 -0500

    model pipelines as hierarchical

--- aircmd/core/artifact.py
+++ aircmd/core/artifact.py
@@ -1,9 +1,8 @@
-from typing import List
 
-import dagger
+from typing import Optional
 
-from ..models.base import GlobalSettings, PipelineContext
-from ..models.click_commands import ClickArgument, ClickCommandMetadata, ClickGroup
+from ..models.base import GlobalSettings, Pipeline, PipelineContext
+from ..models.click_commands import ClickCommandMetadata, ClickGroup
 from ..models.utils import make_pass_decorator
 
 pass_pipeline_context = make_pass_decorator(PipelineContext, ensure=True)
@@ -13,41 +12,35 @@ core_group = ClickGroup(group_name="core", group_help="Commands for developing a
 
 class BuildCommand(ClickCommandMetadata):
     command_name: str = "build"
-    command_help: str = "Build aircmd"
+    command_help: str = "Builds aircmd"
 
-@core_group.command(BuildCommand())
-@pass_pipeline_context
-async def build(ctx: PipelineContext, settings: GlobalSettings) -> None:
-    """Build aircmd"""
-    async with ctx.dagger_connection as client:
-        runner = (
-            client.container(platform=dagger.Platform("linux/amd64"))
-            .from_("python:3.11-buster")
-            .with_directory("/app", client.host().directory(".", include=["./pyproject.toml", "./poetry.lock"]))
-            .with_workdir("/app")
-            .with_exec(["poetry", "install"]) # cache the installation of dependencies so we don't rerun every time we change src
-            .with_directory("/app", client.host().directory(".", include=["./actions", "./aircmd", "./pyproject.toml", "./poetry.lock"]))
-            .with_exec(["poetry", "run", "mypy", "."])
-            .with_exec(["poetry", "run", "ruff", "."])
-            .with_exec(["poetry", "build"])
+async def build_task(ctx: PipelineContext, parent_pipeline: Optional[Pipeline] = None) -> None:
+    pipeline = await Pipeline.create("build", parent_pipeline=parent_pipeline)
+    client = pipeline.dagger_pipeline
+    ctr = ( client
+            .container()
+            .from_("alpine")
+            .with_exec(["apk", "add", "curl"])
+            .with_exec(["curl", "https://dagger.io"])
         )
-        await runner.stdout()
-
-
-
-class TestCommand(ClickCommandMetadata):
-    command_name: str = "test"
-    command_help: str = "Run tests for aircmd"
-    arguments: List[ClickArgument] = [ClickArgument(name="query", required=False)]
-
-
-class PublishCommand(ClickCommandMetadata):
-    command_name: str = "publish"
-    command_help: str = "Publish aircmd to PyPI"
-    arguments: List[ClickArgument] = [ClickArgument(name="query", required=False)]
+    output = await ctr.stdout()
+    print(output[:300])
+    return None
 
 
 class CICommand(ClickCommandMetadata):
     command_name: str = "ci"
     command_help: str = "Run CI for aircmd"
-    arguments: List[ClickArgument] = [ClickArgument(name="query", required=False)]
+
+@core_group.command(BuildCommand())
+@pass_pipeline_context
+async def build(ctx: PipelineContext) -> None:
+    await ctx.run_pipelines([build_task(ctx)], concurrency=1)
+
+@core_group.command(CICommand())
+@pass_pipeline_context
+async def ci(ctx: PipelineContext) -> None:
+    """Run CI for aircmd"""
+    # Creates the 'ci' pipeline as a standalone pipeline.
+    ci_pipeline = await Pipeline.create("ci")
+    await ctx.run_pipelines([build_task(ctx, ci_pipeline)], concurrency=1)
--- aircmd/models/base.py
+++ aircmd/models/base.py
@@ -1,12 +1,13 @@
+import logging
 import os
 import platform
-import subprocess
 import sys
-from typing import Any, Optional, Type
+from glob import glob
+from typing import Any, Coroutine, List, Optional, Type
 
+import anyio
 import dagger
-from dotenv import load_dotenv
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PrivateAttr
 from pydantic_settings import BaseSettings
 
 from ..plugin_manager import PluginManager
@@ -22,8 +23,6 @@ class Singleton:
 
     
 # Immutable. Use this for application configuration. Created at bootstrap.
-
-
 class GlobalSettings(BaseSettings):
     GITHUB_TOKEN: Optional[str] = Field(None, env="GITHUB_TOKEN")
     CI: bool = Field(False, env="CI")
@@ -45,50 +44,143 @@ class GlobalSettings(BaseSettings):
          ),                                                                                                                                                               
          env="PIP_CACHE_DIR"                                                                                                                                              
      ) 
-    # Add new secrets as fields
-    SECRET_1: str = Field(..., env="SECRET_1")
-    SECRET_2: str = Field(..., env="SECRET_2")
+    MYPY_CACHE_DIR: str = Field("~/.cache/.mypy_cache", env="MYPY_CACHE_DIR")
+    DEFAULT_PYTHON_EXCLUDE: List[str] = Field(["**/.venv", "**/__pycache__"], env="DEFAULT_PYTHON_EXCLUDE")
+    DEFAULT_EXCLUDED_FILES: List[str] = Field(
+        [".git"]
+        + glob("**/build", recursive=True)
+        + glob("**/.venv", recursive=True)
+        + glob("**/secrets", recursive=True)
+        + glob("**/__pycache__", recursive=True)
+        + glob("**/*.egg-info", recursive=True)
+        + glob("**/.vscode", recursive=True)
+        + glob("**/.pytest_cache", recursive=True)
+        + glob("**/.eggs", recursive=True)
+        + glob("**/.mypy_cache", recursive=True)
+        + glob("**/.DS_Store", recursive=True)
+    , env="DEFAULT_EXCLUDED_FILES") 
+    DOCKER_VERSION:str = Field("20.10.23", env="DOCKER_VERSION")
+    DOCKER_DIND_IMAGE: str = Field("docker:20-dind", env="DOCKER_DIND_IMAGE")
+    DOCKER_CLI_IMAGE: str = Field("docker:20-cli", env="DOCKER_CLI_IMAGE")
+    GRADLE_CACHE_PATH: str = Field("/root/.gradle/caches", env="GRADLE_CACHE_PATH")
+    GRADLE_BUILD_CACHE_PATH: str = Field(f"{GRADLE_CACHE_PATH}/build-cache-1", env="GRADLE_BUILD_CACHE_PATH")
+    GRADLE_READ_ONLY_DEPENDENCY_CACHE_PATH: str = Field("/root/gradle_dependency_cache", env="GRADLE_READ_ONLY_DEPENDENCY_CACHE_PATH")
+
+    SECRET_DOCKER_HUB_USERNAME: Optional[str] = Field(None, env="SECRET_DOCKER_HUB_USERNAME")
+    SECRET_DOCKER_HUB_PASSWORD: Optional[str] = Field(None, env="SECRET_DOCKER_HUB_PASSWORD")
+    
 
     class Config:                                                                                                                                                        
          arbitrary_types_allowed = True                                                                                                                                   
          env_file = '.env' 
 
-    @classmethod
-    def load_secrets_from_file(cls, secrets_file: str) -> None:
-        """Decrypt the secrets file using SOPS and load it into environment variables."""
-        try:
-            decrypted_secrets = subprocess.check_output(["sops", "-d", secrets_file])
-            with open(".env", "wb") as f:
-                f.write(decrypted_secrets)
-            load_dotenv(".env")
-            os.remove(".env")
-        except subprocess.CalledProcessError as e:
-            raise ValueError(f"Error decrypting secrets file: {e}")
-        except FileNotFoundError:
-            raise ValueError("SOPS command not found. Please install SOPS to use this feature.")
 
-# Mutable. Store pipeline state here. Created at runtime
-class PipelineContext(BaseModel, Singleton):
-    dagger_config: dagger.Config
-    dagger_connection: dagger.Connection
+'''
+In the case of modeling CI pipelines, the child-knows-parent approach seems more suitable for the 
+following reasons:                                                                                
 
-    def __init__(self, config: Optional[dagger.Config] = None,  
-                 connection: Optional[dagger.Connection] = None, 
-                 **data: Any):
-        if config is None:
-            config = dagger.Config(log_output=sys.stdout)
-        if connection is None:
-            # Replace with your logic to create a Connection instance
-            connection = dagger.Connection(config = config)
-        super().__init__(dagger_config=config, dagger_connection=connection, **data)
+ 1 Analyzing individual pipeline runs: When you want to analyze a specific pipeline run, you might
+   need to compare it with its parent or ancestor runs to understand the differences or           
+   improvements. In this case, having a reference to the parent pipeline makes it easier to       
+   traverse up the pipeline hierarchy.                                                            
+ 2 Dependency tracking: In some CI systems, a pipeline run might depend on the successful         
+   completion of its parent or ancestor runs. With the child-knows-parent approach, you can easily
+   check the status of the parent pipelines and determine if the current pipeline run should      
+   proceed or not.                                                                                
+ 3 Debugging and troubleshooting: When a pipeline run fails, you might need to investigate the    
+   cause of the failure by looking at the parent or ancestor runs. The child-knows-parent approach
+   allows you to easily navigate up the pipeline hierarchy to find the relevant information.      
+
+While it's true that some use cases might require traversing the pipeline hierarchy from the root 
+to the leaves, these cases are less common in CI systems. The child-knows-parent approach provides
+more flexibility and ease of use for the typical operations performed on CI pipelines.        
+
+'''
+class Pipeline(BaseModel):
+    name: str
+    parent_pipeline: Optional['Pipeline']
+    dagger_pipeline: dagger.Client
 
     class Config:
-        arbitrary_types_allowed = True
+        arbitrary_types_allowed=True
+
+    @classmethod
+    async def create(cls, name: str, parent_pipeline: Optional['Pipeline'] = None) -> 'Pipeline':
+        if parent_pipeline is None:
+            # This is a top-level pipeline. Get the Dagger client from the current PipelineContext.
+            async with PipelineContext() as ctx:
+                dagger_client = ctx.dagger_client
+        else:
+            # This is a nested pipeline. Get the Dagger client from the parent pipeline.
+            dagger_client = parent_pipeline.dagger_pipeline
+
+        # Create the pipeline using the appropriate Dagger client.
+       
+        dagger_pipeline = dagger_client.pipeline(name)
+        return cls(name=name, parent_pipeline=parent_pipeline, dagger_pipeline=dagger_pipeline)
+
+
+class PipelineContext(BaseModel, Singleton):
+    _current_level: int = 0
+    logger: logging.Logger
+    current_running_tasks: int = 0
+    max_concurrency: int = 0
+    max_concurrency_per_level: dict[int, int] = {}
+    concurrency_lock: anyio.Lock = anyio.Lock()
+    _dagger_client: Optional[dagger.Client] = PrivateAttr()
+    dagger_client: dagger.Client = Field(default=None, init=False)
+
+
+    class Config:
+        arbitrary_types_allowed=True
+
+    
+    async def __aenter__(self) -> 'PipelineContext':
+        self.dagger_client = await dagger.Connection(dagger.Config(log_output=sys.stdout)).__aenter__()
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        if self.dagger_client is not None:
+            pass
+
+
+    async def update_concurrency(self, delta: int, level: int) -> None:
+        async with self.concurrency_lock:
+            self.current_running_tasks += delta
+            current_level_concurrency = self.max_concurrency_per_level.get(level, 0) + delta
+            self.max_concurrency_per_level[level] = max(self.max_concurrency_per_level.get(level, 0), current_level_concurrency)
+            self.max_concurrency = max(self.max_concurrency_per_level.values())
+
+
+    async def run_pipelines(self, pipelines: List[Coroutine], concurrency: int) -> None:
+        semaphore = anyio.Semaphore(concurrency)
+
+        async def run_pipeline_with_semaphore(pipeline: Coroutine) -> None:
+            async with semaphore:
+                await self.update_concurrency(1, self._current_level)
+                await pipeline
+                await self.update_concurrency(-1, self._current_level)
+
+
+        self._current_level += 1
+        async with anyio.create_task_group() as tg:
+            for pipeline in pipelines:
+                tg.start_soon(run_pipeline_with_semaphore, pipeline)
+        self._current_level -= 1
+        print("All tasks completed")
+        print(f"Maximum concurrency observed: {self.max_concurrency}")
+
+    def __init__(self,
+                 **data: Any):
+        logger = logging.getLogger("PipelineContext")
+        super().__init__(logger=logger, **data)
 
 # Mutaable. Store global application state here. Created at runtime
-class GlobalContext(BaseModel,Singleton):
+
+class GlobalContext(BaseModel, Singleton):
     plugin_manager: PluginManager
     pipeline_context: Optional[PipelineContext] = None
+
     class Config:
         arbitrary_types_allowed = True
 
@@ -100,4 +192,7 @@ class GlobalContext(BaseModel,Singleton):
         if plugin_manager is None:
             plugin_manager = PluginManager()
         super().__init__(plugin_manager=plugin_manager, **data)
-        super().__init__(plugin_manager=plugin_manager, **data)
+
+
+
+# Remove the pipeline decorator
