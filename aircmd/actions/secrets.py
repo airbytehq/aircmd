5ca5cb087978e08843677b73fcb15c4903bcf810
cpdeethree <conor@airbyte.io>
2023-06-09 08:44:32 -0500

    fix ruff errors

--- aircmd/actions/secrets.py
+++ aircmd/actions/secrets.py
@@ -1,7 +1,8 @@
-from dotenv import load_dotenv
 import os
 import subprocess
-from typing import Any, Type
+
+from dotenv import load_dotenv
+
 
 def load_secrets_from_file(secrets_file: str) -> None:
     """Decrypt the secrets file using SOPS and load it into environment variables."""
@@ -13,5 +14,7 @@ def load_secrets_from_file(secrets_file: str) -> None:
         os.remove(".env")
     except subprocess.CalledProcessError as e:
         raise ValueError(f"Error decrypting secrets file: {e}")
+    except FileNotFoundError:
+        raise ValueError("SOPS command not found. Please install SOPS to use this feature.")
     except FileNotFoundError:
         raise ValueError("SOPS command not found. Please install SOPS to use this feature.")
