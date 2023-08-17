23b31e46ff7a002ce5caf85153a07e1996f465e4
cpdeethree <conor@airbyte.io>
2023-08-16 19:20:11 -0500

    update Singleton to give control over intialization

--- aircmd/models/base.py
+++ aircmd/models/base.py
@@ -36,8 +36,20 @@ class PipelineContext(BaseModel, Singleton):
         arbitrary_types_allowed=True
 
     def __init__(self, global_settings: GlobalSettings, **data: dict[str, Any]):
-        super().__init__(global_settings=global_settings, **data)
-        self.set_global_prefect_tag_context()
+        """
+        Initialize the PipelineContext instance.
+
+        This method checks the _initialized flag for the PipelineContext class in the Singleton base class.
+        If the flag is False, the initialization logic is executed and the flag is set to True.
+        If the flag is True, the initialization logic is skipped.
+
+        This ensures that the initialization logic is only executed once, even if the PipelineContext instance is retrieved multiple times.
+        This can be useful if the initialization logic is expensive (e.g., it involves network requests or database queries).
+        """
+        if not Singleton._initialized[PipelineContext]:
+            super().__init__(global_settings=global_settings, **data)
+            self.set_global_prefect_tag_context()
+            Singleton._initialized[PipelineContext] = True
     
     import asyncio
 
@@ -48,7 +60,6 @@ class PipelineContext(BaseModel, Singleton):
             async with self._dagger_client_lock:
                 if not self._dagger_client:
                     connection = dagger.Connection(dagger.Config(log_output=sys.stdout))
-                    print("ABC123")
                     self._dagger_client = await self._click_context().with_async_resource(connection) # type: ignore
         client = self._dagger_client
         assert client, "Error initializing Dagger client"
